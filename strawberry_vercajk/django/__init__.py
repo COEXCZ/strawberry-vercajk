b2eae7c18f9552d571b2845eecb77018a6c52c99
LukasBerka <lukasberkacek@gmail.com>
2025-07-16 10:57:30 +0200

    nit

--- strawberry_vercajk/django/__init__.py
+++ strawberry_vercajk/django/__init__.py
@@ -4,6 +4,6 @@ except ImportError:
     raise RuntimeError(
         "Django support in strawberry-vercajk requires the 'django' extra: pip install strawberry-vercajk[django]"
     )
-from strawberry_vercajk._list.django import DjangoListResponseHandler
 
+from strawberry_vercajk._list.django import DjangoListResponseHandler
 from strawberry_vercajk._base.query_logger import QueryLogger
